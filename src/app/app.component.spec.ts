225fa5eaf372eeafb1c32c9a8ff1e0349d8fb1a8
PEDRO HERNANDES AGUIAR <pedro.haguiar@senacsp.edu.br>
2026-09-08 20:45:21 -0300

    primeira versao

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'FintechCrud' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FintechCrud');
+    expect(app.titulo).toEqual('FintechCrud');
   });
 
   it('should render title', () => {
