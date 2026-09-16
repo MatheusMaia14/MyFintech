87ad224112a3bd7f3870fb638fb666496404daa1
MATHEUS LUCAS GONZAGA MAIA <matheus.lgmaia@senacsp.edu.br>
2026-09-15 22:29:48 -0300

    Segunda

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,10 +5,31 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
-})
+})  
 //trecho omitido
 export class LoginComponent {
- onBotaoClicado() {
- alert("Bem-vindo(a)!");
- }
+  login: string = "";
+  senha: string = "";
+  botaoDesabilitado: boolean = true;
+
+  validarFormulario(){
+    if (this.login.trim() !== " && this.senha.trim() ! ==") {
+      this.botaoDesabilitado = false;
+    } else {
+      this.botaoDesabilitado = true;
+    }
+  }
+
+    fazerLogin(){
+      if (this.login === 'admin' && this.senha === '123'){
+        alert('Bem-vindo ${this.login)!')
+      }
+    }
+  onBotaoClicado() {
+    alert("Bem-vindo(a)!");
+  }
+
+  teclaDigitada(event: KeyboardEvent): void {
+    alert(`O usuário digitou: ${event.key}`);
+  }
 }
